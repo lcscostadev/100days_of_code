8678d3a3d7a2f95700c25a2bb2647b6f10fb284a
Lucas Costa <101674510+lcscostadev@users.noreply.github.com>
2022-05-04 20:24:02 -0300

    kl

---
+++ day_24/modularization/app.js
@@ -0,0 +1,4 @@
+import { default as sayHello, sayGoodbye } from './greetings';
+
+sayHello();
+sayGoodbye();
---
+++ day_24/modularization/greetings.js
@@ -0,0 +1,11 @@
+function sayHello() {
+    return 'Hello world';
+}
+function sayGoodbye() {
+    return 'Goodbye world';
+}
+
+module.exports = {
+    sayHello: sayHello,
+    sayGoodbye: sayGoodbye
+};
